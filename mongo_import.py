66aba9e144907bffd478d6baa97f95a9b1cb7d1a
erwijet <sreedoflight@gmail.com>
2019-02-24 14:01:06 -0600

    Removed personal mongoDB address from mongo_import.py

--- mongo_import.py
+++ mongo_import.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 def insert_card_into_owned_collection(cardname: str):
-    CONN_STRING = 'mongodb://dbo:honeyTSH207980@ds127015.mlab.com:27015/cardlib'
+    CONN_STRING = '< your mongoDB url here :) >'
 
     client = MongoClient(CONN_STRING)
     db = client['cardlib']
